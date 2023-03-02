d332b6c13e15a811958508944fd4f2f4fb176aa0
Toptall <toptall0727@gmail.com>
2023-03-03 00:13:30 +0300

    4.Admin Logout Option
    Admin Logout Option

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Auth;
+
 class AdminController extends Controller
 {
     public function AdminDashboard(){
@@ -11,5 +13,15 @@ class AdminController extends Controller
         return view('admin.index');
 
     } // End Method 
+
+    public function AdminLogout(Request $request){
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
+    }// End Method 
 }
  
--- routes/web.php
+++ routes/web.php
@@ -35,7 +35,7 @@ require __DIR__.'/auth.php';
 Route::middleware(['auth','role:admin'])->group(function(){
 
  Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard'); 
-
+ Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout'); 
 }); // End Group Admin Middleware
   
 
