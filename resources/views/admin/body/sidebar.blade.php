09a5f5f602a40c20e2d4bdcae124c6024aa90bc2
Toptall <toptall0727@gmail.com>
2023-03-03 00:30:13 +0300

    6.Refreach Admin Template
    Refreach Admin Template

--- resources/views/admin/body/sidebar.blade.php
+++ resources/views/admin/body/sidebar.blade.php
@@ -13,7 +13,7 @@
       <ul class="nav">
         <li class="nav-item nav-category">Main</li>
         <li class="nav-item">
-          <a href="dashboard.html" class="nav-link">
+          <a href="{{ route('admin.dashboard') }}" class="nav-link">
             <i class="link-icon" data-feather="box"></i>
             <span class="link-title">Dashboard</span>
           </a>
@@ -66,69 +66,8 @@
               <li class="nav-item">
                 <a href="pages/ui-components/alerts.html" class="nav-link">Alerts</a>
               </li>
-              <li class="nav-item">
-                <a href="pages/ui-components/badges.html" class="nav-link">Badges</a>
-              </li>
-              <li class="nav-item">
-                <a href="pages/ui-components/breadcrumbs.html" class="nav-link">Breadcrumbs</a>
-              </li>
-              <li class="nav-item">
-                <a href="pages/ui-components/buttons.html" class="nav-link">Buttons</a>
-              </li>
-              <li class="nav-item">
-                <a href="pages/ui-components/button-group.html" class="nav-link">Button group</a>
-              </li>
-              <li class="nav-item">
-                <a href="pages/ui-components/cards.html" class="nav-link">Cards</a>
-              </li>
-              <li class="nav-item">
-                <a href="pages/ui-components/carousel.html" class="nav-link">Carousel</a>
-              </li>
-              <li class="nav-item">
-                  <a href="pages/ui-components/collapse.html" class="nav-link">Collapse</a>
-                </li>
-              <li class="nav-item">
-                <a href="pages/ui-components/dropdowns.html" class="nav-link">Dropdowns</a>
-              </li>
-              <li class="nav-item">
-                <a href="pages/ui-components/list-group.html" class="nav-link">List group</a>
-              </li>
-              <li class="nav-item">
-                <a href="pages/ui-components/media-object.html" class="nav-link">Media object</a>
-              </li>
-              <li class="nav-item">
-                <a href="pages/ui-components/modal.html" class="nav-link">Modal</a>
-              </li>
-              <li class="nav-item">
-                <a href="pages/ui-components/navs.html" class="nav-link">Navs</a>
-              </li>
-              <li class="nav-item">
-                <a href="pages/ui-components/navbar.html" class="nav-link">Navbar</a>
-              </li>
-              <li class="nav-item">
-                <a href="pages/ui-components/pagination.html" class="nav-link">Pagination</a>
-              </li>
-              <li class="nav-item">
-                <a href="pages/ui-components/popover.html" class="nav-link">Popovers</a>
-              </li>
-              <li class="nav-item">
-                <a href="pages/ui-components/progress.html" class="nav-link">Progress</a>
-              </li>
-              <li class="nav-item">
-                <a href="pages/ui-components/scrollbar.html" class="nav-link">Scrollbar</a>
-              </li>
-              <li class="nav-item">
-                <a href="pages/ui-components/scrollspy.html" class="nav-link">Scrollspy</a>
-              </li>
-              <li class="nav-item">
-                <a href="pages/ui-components/spinners.html" class="nav-link">Spinners</a>
-              </li>
-              <li class="nav-item">
-                <a href="pages/ui-components/tabs.html" class="nav-link">Tabs</a>
-              </li>
-              <li class="nav-item">
-                <a href="pages/ui-components/tooltips.html" class="nav-link">Tooltips</a>
-              </li>
+              
+              
             </ul>
           </div>
         </li>
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -136,41 +136,7 @@
         </div> <!-- row -->
 
         <div class="row">
-          <div class="col-12 col-xl-12 grid-margin stretch-card">
-            <div class="card overflow-hidden">
-              <div class="card-body">
-                <div class="d-flex justify-content-between align-items-baseline mb-4 mb-md-3">
-                  <h6 class="card-title mb-0">Revenue</h6>
-                  <div class="dropdown">
-                    <a type="button" id="dropdownMenuButton3" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                      <i class="icon-lg text-muted pb-3px" data-feather="more-horizontal"></i>
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton3">
-                      <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i data-feather="eye" class="icon-sm me-2"></i> <span class="">View</span></a>
-                      <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i data-feather="edit-2" class="icon-sm me-2"></i> <span class="">Edit</span></a>
-                      <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i data-feather="trash" class="icon-sm me-2"></i> <span class="">Delete</span></a>
-                      <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i data-feather="printer" class="icon-sm me-2"></i> <span class="">Print</span></a>
-                      <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i data-feather="download" class="icon-sm me-2"></i> <span class="">Download</span></a>
-                    </div>
-                  </div>
-                </div>
-                <div class="row align-items-start">
-                  <div class="col-md-7">
-                    <p class="text-muted tx-13 mb-3 mb-md-0">Revenue is the income that a business has from its normal business activities, usually from the sale of goods and services to customers.</p>
-                  </div>
-                  <div class="col-md-5 d-flex justify-content-md-end">
-                    <div class="btn-group mb-3 mb-md-0" role="group" aria-label="Basic example">
-                      <button type="button" class="btn btn-outline-primary">Today</button>
-                      <button type="button" class="btn btn-outline-primary d-none d-md-block">Week</button>
-                      <button type="button" class="btn btn-primary">Month</button>
-                      <button type="button" class="btn btn-outline-primary">Year</button>
-                    </div>
-                  </div>
-                </div>
-                <div id="revenueChart" ></div>
-              </div>
-            </div>
-          </div>
+          
         </div> <!-- row -->
 
         <div class="row">
