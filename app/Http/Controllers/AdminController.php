43d7d4b0f1f307ba04629c395b4046ad52b22bd1
Toptall <toptall0727@gmail.com>
2023-03-04 00:54:10 +0300

    8.Update Admin Assets Path 2
    Update Admin Assets Path

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class AdminController extends Controller
 {
@@ -29,5 +30,8 @@ class AdminController extends Controller
         return view('admin.admin_login');
 
     }// End Method 
+
+   
+
 }
  
