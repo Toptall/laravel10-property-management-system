6418c5a58eb000eea7f95fc9afd8060edbd5b762
Toptall <toptall0727@gmail.com>
2023-03-04 01:23:04 +0300

    13.Admin Profile Change Password Part 1
    Admin Profile Change Password Part 1

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -67,5 +67,13 @@ class AdminController extends Controller
 
      }// End Method 
 
+     public function AdminChangePassword(){
+
+        $id = Auth::user()->id;
+       $profileData = User::find($id);
+       return view('admin.admin_change_password',compact('profileData'));
+
+    }// End Method 
+
 }
  
