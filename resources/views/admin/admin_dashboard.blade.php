d64d21e08895cd5dbc78ef1259ecc9276413da86
Toptall <toptall0727@gmail.com>
2023-03-07 00:32:16 +0300

    26.Property Type Crud Part 2
    Property Type Crud Part 2

--- resources/views/admin/admin_dashboard.blade.php
+++ resources/views/admin/admin_dashboard.blade.php
@@ -23,6 +23,9 @@
   <!-- Plugin css for this page -->
   <link rel="stylesheet" href="{{ asset('backend/assets/vendors/flatpickr/flatpickr.min.css') }}">
   <!-- End plugin css for this page -->
+  <!-- Plugin css for this page -->
+  <link rel="stylesheet" href="{{ asset('backend/assets/vendors/datatables.net-bs5/dataTables.bootstrap5.css') }}">
+  <!-- End plugin css for this page -->
 
   <!-- inject:css -->
   <link rel="stylesheet" href="{{ asset('backend/assets/fonts/feather-font/css/iconfont.css') }}">
@@ -77,6 +80,12 @@
         <!-- Custom js for this page -->
         <script src="{{ asset('backend/assets/js/dashboard-dark.js') }}"></script>
         <!-- End custom js for this page -->
+        <!-- Start datatables -->
+        <script src="{{ asset('backend/assets/vendors/datatables.net/jquery.dataTables.js') }}"></script>
+        <script src="{{ asset('backend/assets/vendors/datatables.net-bs5/dataTables.bootstrap5.js') }}"></script> 
+        <script src="{{ asset('backend/assets/js/data-table.js') }}"></script>
+        <!-- End datatables -->
+
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 
         <script>
